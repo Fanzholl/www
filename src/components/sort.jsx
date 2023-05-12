21a34f64cadaa06a4e0a22a5e8961741cf1ee50b
Fanzholl <58570733+Fanzholl@users.noreply.github.com>
2023-05-12 20:52:35 +0300

    Поменял код для реализации сортировки.

--- src/components/sort.jsx
+++ src/components/sort.jsx
@@ -1,10 +1,17 @@
 import React from 'react';
 import './styles/sort.css';
 
-const Sort = () => {
+const Sort = (properties) => {
+    function DownSale() {
+        properties.setCardData(properties.cardData.sort( (a, b) => {
+            return a.price - b.price
+        }));
+
+        console.log(properties.cardData)
+    }
     return (
         <div className={"Sort"}>
-            <p>По цене</p>
+            <p id={"DownSale"} onClick={DownSale}>По цене</p>
             <img src={"./images/up.svg"} alt=""/>
             <p>По цене</p>
             <img src={"./images/down.svg"} alt=""/>
