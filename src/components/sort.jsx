c3c81c3334c1e40291eb77477863288f8f3f0da1
Fanzholl <58570733+Fanzholl@users.noreply.github.com>
2023-05-12 21:01:56 +0300

    Реализована сортировка.

--- src/components/sort.jsx
+++ src/components/sort.jsx
@@ -3,17 +3,25 @@ import './styles/sort.css';
 
 const Sort = (properties) => {
     function DownSale() {
-        properties.setCardData(properties.cardData.sort( (a, b) => {
+
+        properties.setCardData( prev => [...prev].sort( (a, b) => {
             return a.price - b.price
         }));
 
-        console.log(properties.cardData)
+    }
+
+    function UpSale() {
+
+        properties.setCardData( prev => [...prev].sort( (a, b) => {
+            return b.price - a.price
+        }));
+
     }
     return (
         <div className={"Sort"}>
-            <p id={"DownSale"} onClick={DownSale}>По цене</p>
+            <p onClick={UpSale}>По цене</p>
             <img src={"./images/up.svg"} alt=""/>
-            <p>По цене</p>
+            <p onClick={DownSale}>По цене</p>
             <img src={"./images/down.svg"} alt=""/>
             <p>Сначала новинки</p>
         </div>
